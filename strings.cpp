1d1f6a22bc5198ea297fd5b483c6332fe9e59c5c
SamarthBajpaii <samarthbajpai6@gmail.com>
2026-08-04 15:09:41 +0000

    Day 7 : Strings basics

---
+++ strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    char S[]={"Hello yo"};
+    char St[]={'H','e','l','l','o','\0'};
+    char Str[]= {66,67,68,69,70};
+    cout<<S<<endl;
+    cout<<St;
+    cout<<Str;
+
+    return 0;
+      
+}
